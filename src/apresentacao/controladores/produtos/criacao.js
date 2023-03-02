11d8606b113d27ef76528d7d916995968d8dfcd4
mateus <mateusflorencio@outlook.com.br>
2023-03-02 00:10:30 -0300

    fix: pegando informacoes apenas do body

--- src/apresentacao/controladores/produtos/criacao.js
+++ src/apresentacao/controladores/produtos/criacao.js
@@ -1,9 +1,9 @@
 import { badRequest, created, serverError } from '@/apresentacao/http.js'
 
-export default (validar, sanitizar, casoDeUsoCriacaoProduto) => async (body) => {
+export default (validar, sanitizar, casoDeUsoCriacaoProduto) => async ({ body }) => {
   try {
     const erros = validar(body)
-    if (erros.length > 0) return badRequest(erros)
+    if (erros?.length > 0) return badRequest(erros)
     const produto = sanitizar(body)
     const resultado = await casoDeUsoCriacaoProduto(produto)
     if (resultado.erros) return badRequest(resultado.erros)
