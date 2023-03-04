f3789abe9047b0b872dc3e2840879105f65d430d
mateus <mateusflorencio@outlook.com.br>
2023-03-03 22:54:59 -0300

    feat: add mapeamento para retorno do mongo

--- src/externo/repositorio/mongo/produtos.js
+++ src/externo/repositorio/mongo/produtos.js
@@ -1,14 +1,25 @@
 export default (single) => ({
   criar: async (produto) => {
-    const data = await single.produtos.create({ produto })
-    return data
+    const data = await single.produtos.create(produto)
+    return map(data)
   },
   buscaComFiltro: async ({ ord, dir, page, limit, field, search }) => {
     const filtros = field && search ? { [field]: { $regex: search, $options: 'i' } } : {}
     const sort = { [ord]: dir === 'asc' ? 1 : -1 }
     const skip = (page - 1) * limit
-    const fields = { _id: 0, __v: 0 }
+    const fields = { __v: 0 }
     const data = await single.produtos.find(filtros, fields).sort(sort).skip(skip).limit(limit)
-    return data
+    return map(data)
   }
 })
+
+const map = (data) => {
+  if (!data) return null
+  if (Array.isArray(data)) return data.map(map)
+  return {
+    id: data._id,
+    nome: data.nome,
+    preco: data.preco,
+    descricao: data.descricao
+  }
+}
